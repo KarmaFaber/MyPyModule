15ad8b5802c9e8ac017afb3b5719e4f74cba2fd7
KarmaFaber <maria.zolotarova@gmail.com>
2020-01-14 01:10:11 +0100

    vers 1.1

---
+++ README.md
@@ -0,0 +1,35 @@
+# MyPyModule
+
+Sign up project in pro and basic version.
+
+## Built With
+
+* [Visual Studio Code](https://code.visualstudio.com/) - Windows 10 version.  
+
+
+### Project parts
+* qr_code_creator
+* sql
+* random_string_creator
+
+### Python libraries used
+* qrcode
+* random
+* datetime
+* string 
+* random
+* pymysql
+
+
+## Contributing
+This project is made for educational purposes, GitHub users are free to download and use it freely.
+
+## Author
+
+* **Karma Faber** 
+
+## Versions:
+* <b>version 1.0</b> - Operational Version. 
+
+
+
--- qr_code_creator.py
+++ qr_code_creator.py
@@ -12,11 +12,13 @@
 #create_QR_code(data) -->  data="https://github.com/KarmaFaber"
 
 import qrcode
-from MyPyModule.random_string import id_generator
+from random_string_creator import id_generator
+from pillow import Image, ImageDraw, ImageFont
+import random
+import datetime
 
 def create_QR_code(data):
     qr=qrcode.QRCode(version=1,error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
-
 
     data=""
     qr.add_data(data)
@@ -28,3 +30,68 @@ def create_QR_code(data):
 
     img.save(img_name) 
     return img
+
+
+
+
+
+
+def id_creator(name, lastname, passport, company):
+    #image:
+    image = Image.new('RGB', (1000,900), (255, 255, 255))
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype('arial.ttf', size=45)
+
+    #date and time:
+    d_date = datetime.datetime.now()
+    reg_format_date = d_date.strftime("  %d-%m-%Y\t\t ID CARD Generator\t\t\t\t\t  %I:%M:%S %p")
+
+    #date, name, lastname, passport, company:---
+    (x, y) = (50, 250)
+    message = ""
+    date=message
+    color = 'rgb(0, 0, 0)'
+    font = ImageFont.truetype('arial.ttf', size=45)
+    draw.text((x, y), message, fill=color, font=font)
+
+    (x, y) = (50, 350)
+    message = ""
+    name=message
+    color = 'rgb(0, 0, 0)'
+    font = ImageFont.truetype('arial.ttf', size=45)
+    draw.text((x, y), message, fill=color, font=font)
+
+    (x, y) = (600, 75)
+    idno=random.randint(10000000,90000000)
+    message = str('ID '+str(idno))
+    color = 'rgb(89, 62, 100)' # black color
+    font = ImageFont.truetype('arial.ttf', size=60)
+    draw.text((x, y), message, fill=color, font=font)
+
+    (x, y) = (50, 450)
+    message = ""
+    lastname=message
+    color = 'rgb(0, 0, 0)'
+    font = ImageFont.truetype('arial.ttf', size=45)
+    draw.text((x, y), message, fill=color, font=font)
+    
+    (x, y) = (50, 250)
+    message = ""
+    passport=message
+    color = 'rgb(0, 0, 0)'
+    font = ImageFont.truetype('arial.ttf', size=45)
+    draw.text((x, y), message, fill=color, font=font)
+
+    # save the edited image
+    image.save(str(name)+'.png')
+
+    img = qrcode.make(str(company)+str(idno))   
+    img.save(str(idno)+'.bmp')
+
+    til = Image.open(name+'.png')
+    im = Image.open(str(idno)+'.bmp') 
+    til.paste(im,(600,350))
+    til.save(name+'.png')
+
+    
+print(id_creator("maria", "karma", "jajdbkj", "sinfatinogas"))
